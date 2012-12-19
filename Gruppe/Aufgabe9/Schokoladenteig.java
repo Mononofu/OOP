3f684124e0cf972d4f808bdca2f518d53ecdf68c
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-12-19 10:57:20 +0100

    rant

--- Gruppe/Aufgabe9/Schokoladenteig.java
+++ Gruppe/Aufgabe9/Schokoladenteig.java
@@ -1,9 +1,12 @@
+/* in a better language: 'case object Schokoladenteig extends Teig' */
+
 public class Schokoladenteig implements Teig {
   @Override public String toString() {
   	return "Schokoladenteig";
   }
 
   /* alle Schokoteige sind gleich */
+  /* this is necessary since in java it's not possible to directly declare objects */
   @Override public boolean equals(Object o) {
     return o instanceof Schokoladenteig;
   }
